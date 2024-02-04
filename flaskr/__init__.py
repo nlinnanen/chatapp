dbf39d2f5d4656a4baa61adfe1be13c542488e1d
Niklas Linnanen <nlinnanen@gmail.com>
2024-02-04 14:46:49 +0200

    Redirect to login if not logged in

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, request
 
 app = Flask(__name__)
 
 from . import routes
+
+WHITELISTED_PATHS = ['/login', '/static']
+
+@app.before_request
+def check_user_logged_in():
+    if "username" not in session and request.path not in WHITELISTED_PATHS:
+        return redirect('/login')
--- flaskr/routes/__init__.py
+++ flaskr/routes/__init__.py
@@ -1,13 +1,10 @@
 from flaskr import app
 from ..db import db
-from flask import render_template, request
+from flask import render_template, session, redirect
 from sqlalchemy.sql import text
 
 @app.route("/")
 def index():
-    print(type(db))
-    result = db.session.execute(text("SELECT message, username FROM messages INNER JOIN users ON messages.sender_id = users.id ORDER BY messages.created_at"))
-    messages = result.fetchall()
-    return render_template("index.html", messages=messages) 
+    return render_template("index.html")
 
-from . import categories, messages, users, conversations 
+from . import login, categories, messages, users, conversations 
