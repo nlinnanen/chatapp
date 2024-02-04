fb453f437111e1d79ddf632c11768e77970d7ebc
Niklas Linnanen <nlinnanen@gmail.com>
2024-02-04 23:21:47 +0200

    Show username instead of id in message listing

--- flaskr/db/messages.py
+++ flaskr/db/messages.py
@@ -1,27 +1,39 @@
 from . import db
 from sqlalchemy.sql import text
 
+
 def create_message(message, conversation_id, sender_id):
     sql = text("INSERT INTO messages (message, conversation_id, sender_id) VALUES (:message, :conversation_id, :sender_id) RETURNING *")
-    result = db.session.execute(sql, {"message":message, "conversation_id":conversation_id, "sender_id":sender_id})
+    result = db.session.execute(
+        sql, {"message": message, "conversation_id": conversation_id, "sender_id": sender_id})
     db.session.commit()
     return result.fetchone()
 
+
 def get_messages(conversation_id):
-    result = db.session.execute(text("SELECT * FROM messages WHERE conversation_id = :conversation_id"), {"conversation_id":conversation_id})
+    result = db.session.execute(text('''
+        SELECT *
+        FROM messages LEFT JOIN users ON messages.sender_id = users.id
+        WHERE conversation_id = :conversation_id
+    '''), {"conversation_id": conversation_id})
     return result.fetchall()
 
+
 def get_message(id):
-    result = db.session.execute(text("SELECT * FROM messages WHERE id = :id"), {"id":id})
+    result = db.session.execute(
+        text("SELECT * FROM messages WHERE id = :id"), {"id": id})
     return result.fetchone()
+
 
 def update_message(id, message):
     sql = text("UPDATE messages SET message = :message WHERE id = :id RETURNING *")
-    result = db.session.execute(sql, {"message":message, "id":id})
+    result = db.session.execute(sql, {"message": message, "id": id})
     db.session.commit()
     return result.fetchone()
 
+
 def delete_message(id):
-    result = db.session.execute(text("DELETE FROM messages WHERE id = :id RETURNING *"), {"id":id})
+    result = db.session.execute(
+        text("DELETE FROM messages WHERE id = :id RETURNING *"), {"id": id})
     db.session.commit()
     return result.fetchone()
